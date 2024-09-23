465014d45ecd4843e23e04e00c3c62092832d81d
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-09-23 13:59:42 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -28,7 +28,7 @@
           <a class="nav-link" href="get.php">Get</a>
         </li>        <li class="nav-item">
           <a class="nav-link" href="get-result.php">Get result</a>
-        </li>
+        </li>       <li class="nav-item">
       </ul>
     </div>
   </div>
