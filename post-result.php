ef4be6ebab9ae1aa4d8b2268fa842ca2e090bece
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-09-22 21:48:13 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,11 +2,11 @@
 $pageTitle="Post-result";
 include "view-header.php";
 ?>
-    <h1>Post-result</h1>
+    <h1>Post result</h1>
   <?php
 if (isset ($_POST['my -name'])) {
   ?>
-  <p>The value sent is : </p>
+  <p>The value sent is:</p>
   <?php
   echo $_POST['my-name'];
 } else{
